6678c9cc794ddca3bae123cd09cade24cdf6ef5d
Rivalser <97056846+Rivalser@users.noreply.github.com>
2025-02-28 19:48:06 +0100

    Alapok alapja
    Rétegek kijavítva, a domb megy, a dekorációk jók akár több rétegben is a menőbb érzethez, a fák jók és rétegződnek. Domb probléma megoldva.
    Az enemy grafikája és animációi ki lettek cserélve, de a unity úgy gondolta hogy ami tegnap jó volt ma már nem így vmiért nem jó. További boncolást kíván az ügy.
    Sebző-mérgező növény hozzáadva.

--- Assets/Scripts/TilemapScripts/Elevation_Entry.cs
+++ Assets/Scripts/TilemapScripts/Elevation_Entry.cs
@@ -1,34 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps; // Tilemap kezeléséhez kell!
 
 public class Elevation_Entry : MonoBehaviour
 {
-
     public Collider2D[] mountainColliders;
     public Collider2D[] boundaryColliders;
-
+    public GameObject treesParent; // A Trees objektum, amit Inspectorban kell beállítani
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-       if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
-           
-            foreach(Collider2D mountain in mountainColliders)
+            foreach (Collider2D mountain in mountainColliders)
             {
-                //Debug.Log("Ütközés történt"); 
                 mountain.enabled = false;
             }
 
-            foreach(Collider2D boundary in boundaryColliders)
+            foreach (Collider2D boundary in boundaryColliders)
             {
-                //Debug.Log("Ütközés történt"); 
                 boundary.enabled = true;
             }
 
+            // Játékos sorting order módosítása
             collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 15;
+
+            // Összes Tilemap sorting order módosítása a "Trees" objektumban
+            if (treesParent != null)
+            {
+                Tilemap[] tilemaps = treesParent.GetComponentsInChildren<Tilemap>(); // Lekér minden Tilemap-et a Trees-ben
+
+                foreach (Tilemap tilemap in tilemaps)
+                {
+                    tilemap.GetComponent<TilemapRenderer>().sortingOrder = 15;
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Trees objektum nincs beállítva az Inspectorban!");
+            }
         }
-
     }
-
 }
